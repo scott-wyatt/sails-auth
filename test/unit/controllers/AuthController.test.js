3c04bf79ba86f979407797e9709e520537c82e5f
Scott Wyatt <scottwyatt86@gmail.com>
2015-04-21 13:55:44 -0400

    Update testing

--- test/unit/controllers/AuthController.test.js
+++ test/unit/controllers/AuthController.test.js
@@ -1,6 +1,5 @@
 var assert = require('assert');
 var request = require('supertest');
-var socket = require('sails.io.js');
 
 describe('Auth Controller', function () {
 
@@ -52,40 +51,62 @@ describe('Auth Controller', function () {
     });
 
     //Test with Web Sockets from sails.io
+    describe('sails.socket', function () {
 
-    it ('passport-local authentication via web socket should succeed if email and password valid', function (done) {
-      
-      io.socket.post('/auth/local', {
-          identifier: 'existing.user@email.com',
-          password: 'admin1234'
-      }, function (resData) {
-          done(resData);
+      describe('With default settings', function() {
+
+        describe('once connected, socket', function () {
+
+          it ('passport-local authentication via web socket should succeed if email and password valid', function (done) {
+            
+            request(sails.hooks.http.app)
+              .post('/auth/local')
+              .send({
+                identifier: 'existing.user@email.com',
+                password: 'admin1234'
+              })
+              .expect(200)
+              .end(function(err) {
+                done(err);
+              });
+
+          });
+
+          it ('passport-local authentication via web socket should fail and return error code if email is invalid', function (done) {
+            
+            request(sails.hooks.http.app)
+              .post('/auth/local')
+              .send({
+                identifier: 'invalid@email.com',
+                password: 'admin1234'
+              })
+              .expect(403)
+              .end(function(err) {
+                done(err);
+              });
+
+          });
+
+          it ('passport-local authentication via web socket should fail and return error code if password is invalid', function (done) {
+
+            request(sails.hooks.http.app)
+              .post('/auth/local')
+              .send({
+                identifier: 'existing.user@email.com',
+                password: 'invalid1235'
+              })
+              .expect(403)
+              .end(function(err) {
+                done(err);
+            });
+
+          });
+
+        });
+
       });
-
     });
-
-    it ('passport-local authentication via web socket should fail and return error code if email is invalid', function (done) {
-      
-      io.socket.post('/auth/local', {
-          identifier: 'invalid@email.com',
-          password: 'admin1234'
-      }, function (resData) {
-          done(resData);
-      });
-
-    });
-
-    it ('passport-local authentication via web socket should fail and return error code if password is invalid', function (done) {
-
-      io.socket.post('/auth/local', {
-          identifier: 'existing.user@email.com',
-          password: 'invalid1234'
-      }, function (resData) {
-          done(resData);
-      });
-
-    });
-
+  
   });
 
 });
